d1401ee5cf5a80e3b4fd9a809ea446de99eb5022
Wannajak <fou1234@hotmail.com>
2016-04-04 03:08:56 +1000

    First upload file
    Work from WK-pc

---
+++ manu.js
@@ -0,0 +1,31 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+document.write(' <div id="logo"><img src="images/Logo.jpg" width="900" height="142" alt=""/></div>');
+document.write(' <link rel="stylesheet" type="text/css" href="style.css"/>');
+document.write(' <div id="container">');
+document.write(' <nav> <ul>');
+document.write(' <li><a href="index.html">HOME</a>\n\</li>');
+
+document.write(' <li><a>Information</a><ul>');
+document.write(' <li><a href="AboutCosplay.html">About Cosplay</a></li>');
+document.write(' <li><a href="PartyGuidelines.html">Party Guidelines</a></li>');
+document.write(' </ul></li>');
+
+document.write(' <li><a>Join Us</a><ul>');
+document.write(' <li><a href="SignUp.html">Sign Up</a></li>');
+document.write(' <li><a href="PriceList.html">Price List</a></li>');
+document.write(' </ul></li>');
+
+document.write(' <li><a href="Announcement.html">Announcement</a></li>');
+
+document.write(' <li><a>Abount Us</a><ul>');
+document.write(' <li><a href="who.html">Who\'s who</a></li>');
+document.write(' <li><a href="Contact.html">Contact Us</a></li>');
+document.write(' <li><a href="OtherSite.html">Other Site</a></li>');
+document.write(' </ul></li>');
+
+document.write(' </ul></nav><br/></div>');
+
